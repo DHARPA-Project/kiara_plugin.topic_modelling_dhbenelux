6846e304fb2df4d57ee9b073ef0e3950fa950b10
Mariella CC <MariellaCC@users.noreply.github.com>
2022-05-23 11:14:46 +0200

    add lda in non working state for now

--- examples/streamlit/app.py
+++ examples/streamlit/app.py
@@ -7,7 +7,8 @@ from multipage import MultiPage
 from pages import (
     data_onboarding,
     tokenization,
-    text_preprocessing
+    text_preprocessing,
+    lda
 )
 
 app = MultiPage()
@@ -17,6 +18,7 @@ app = MultiPage()
 app.add_page('1. Data Onboarding', data_onboarding.app)
 app.add_page('2. Tokenization', tokenization.app)
 app.add_page('3. Remove stop words', text_preprocessing.app)
+app.add_page('4. LDA', lda.app)
 
 # The main app
 app.run()
---
+++ examples/streamlit/pages/lda.py
@@ -0,0 +1,98 @@
+import streamlit as st
+from kiara import Kiara
+from kiara.interfaces.python_api.operation import KiaraOperation
+from kiara_plugin.language_processing.language_processing.tokens import get_stopwords
+import pandas as pd
+
+def app():
+
+    kiara = st.session_state["kiara"]
+
+    compute_coherence = st.checkbox("Compute coherence")
+    if not compute_coherence:
+        number_of_topics_min = st.number_input('Number of topics', min_value = 1, value=7)
+        number_of_topics_max = number_of_topics_min
+    else:
+        number_of_topics_min = st.number_input('Min number of topics', min_value = 1, value=4)
+        number_of_topics_max = st.number_input('Max number of topics', min_value=1, value=7)
+
+    button = st.button("Generate topics")
+    if button:
+
+        print(compute_coherence)
+        
+        table_value = kiara.data_registry.get_value('alias:tokens_array')
+        
+        op = KiaraOperation(kiara=kiara, operation_name="generate.LDA.for.tokens_array")
+        inputs = {
+            'tokens_array': table_value,
+            'num_topics_min': number_of_topics_min,
+            'num_topics_max': number_of_topics_max,
+            'compute_coherence': compute_coherence
+            }
+        job_id = op.queue_job(**inputs)
+
+        try:
+             op.save_result(
+            job_id=job_id, aliases={'topic_models': 'topic_models', 'coherence_table': 'coherence_table', 'coherence_map': 'coherence_map'}
+        )
+        except Exception:
+            pass
+
+    
+
+    # topic_models = kiara.data_registry.get_value('alias:topic_models')
+    # coherence_table = kiara.data_registry.get_value('alias:coherence_table')
+    # coherence_map = kiara.data_registry.get_value('alias:coherence_map')
+
+    # st.write("### Coherence score")
+    # if not compute_coherence:
+    #     st.write("Coherence not considered.")
+    # else:
+    #     if not coherence_map.is_none:
+    #         c_map = coherence_map.get_value_data()
+            
+    #         df_coherence = pd.DataFrame(c_map.keys(), columns=['Number of topics'])
+    #         df_coherence['Coherence'] = c_map.values()
+
+
+    #         st.vega_lite_chart(df_coherence, {
+    #             "mark": {"type": "line", "point": True, "tooltip": True},
+    #             "encoding": {
+    #                 "x": {"field": "Number of topics", "type": "quantitative", "axis": {"format": ".0f", "tickCount": len(df_coherence)-1}},
+    #                 "y": {"field": "Coherence", "type": "quantitative", "format": ".3f"}
+    #             }
+                
+    #         }, use_container_width=True)
+
+
+    #         st.table(df_coherence)
+    #         save = st.checkbox("Save coherence table")
+    #         if save:
+    #             alias = st.text_input("Alias")
+    #             save_btn = st.button("Save")
+    #             if save_btn:
+    #                 if not alias:
+    #                     st.info("Not saving table, no alias provided.")
+    #                 else:
+    #                     saved = coherence_table.save(aliases=[alias])
+    #                     st.info(f"Coherence table saved with alias '{alias}', value id: {saved.id}")
+
+    #     else:
+    #         st.write("No coherence computed (yet).")
+
+    # st.write("### Model details")
+    # if not topic_models.item_is_valid():
+    #     st.write("No models available (yet).")
+    # else:
+    #     all_topic_models = topic_models.get_value_data()
+    #     if not compute_coherence:
+    #         selected_model_idx = number_of_topics_min
+    #     else:
+    #         selected_model_idx = st.selectbox("Number of topics", options=range(number_of_topics_min, number_of_topics_max+1))
+
+    #     try:
+    #         selected_model_table = all_topic_models[selected_model_idx]
+    #         st.table(selected_model_table.to_pandas())
+    #     except KeyError:
+    #         st.write(f"No model for {selected_model_idx} number of topics.")
